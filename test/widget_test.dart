223d7787132e92e2b4069c0aff198514b19c4a35
Enes Cümbüş <149635681+EnesCumbus@users.noreply.github.com>
2024-11-04 10:39:47 +0300

    dfgdsf
    kemal

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,10 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-<<<<<<<<< Temporary merge branch 1
-
-=========
->>>>>>>>> Temporary merge branch 2
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
